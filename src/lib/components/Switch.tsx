65b2b9897750d0c6cf31255e22eaf3ef0aae0f9d
koki-develop <kou.pg.0131@gmail.com>
2024-03-27 21:26:20 +0900

    Update Switch component with label support

--- src/lib/components/Switch.tsx
+++ src/lib/components/Switch.tsx
@@ -6,19 +6,26 @@ export type SwitchProps = {
   onChange: (checked: boolean) => void;
 };
 
-export default function Switcha({ label, checked, onChange }: SwitchProps) {
+export default function Switch({ label, checked, onChange }: SwitchProps) {
   return (
-    <HeadlessSwitch
-      className={`${
-        checked ? "bg-blue-600" : "bg-gray-200"
-      } relative inline-flex h-6 w-11 items-center rounded-full`}
-      checked={checked}
-      onChange={onChange}>
-      <span
-        className={`${
-          checked ? "translate-x-6" : "translate-x-1"
-        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-      />
-    </HeadlessSwitch>
+    <HeadlessSwitch.Group>
+      <div className="flex items-center gap-2 whitespace-nowrap">
+        <HeadlessSwitch
+          className={`${
+            checked ? "bg-blue-600" : "bg-gray-200"
+          } relative inline-flex h-6 w-11 items-center rounded-full`}
+          checked={checked}
+          onChange={onChange}>
+          <span
+            className={`${
+              checked ? "translate-x-6" : "translate-x-1"
+            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </HeadlessSwitch>
+        <HeadlessSwitch.Label className="cursor-pointer">
+          {label}
+        </HeadlessSwitch.Label>
+      </div>
+    </HeadlessSwitch.Group>
   );
 }
