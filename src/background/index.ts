4031599db633e319308ff9e5fe7a6c449e497206
koki-develop <kou.pg.0131@gmail.com>
2024-03-27 20:54:17 +0900

    Add background script for AWS masking settings

---
+++ src/background/index.ts
@@ -0,0 +1,38 @@
+import { Storage } from "@plasmohq/storage";
+
+chrome.storage.onChanged.addListener((changes) => {
+  console.log({ changes });
+  chrome.tabs.query(
+    {
+      url: [
+        "https://aws.amazon.com/*",
+        "https://*.aws.amazon.com/*",
+        "https://*.awsapps.com/*"
+      ]
+    },
+    (tabs) => {
+      for (const [key, change] of Object.entries(changes)) {
+        if (key === "AWS_MASKING_SETTINGS") {
+          for (const tab of tabs) {
+            chrome.tabs.sendMessage(tab.id, {
+              method: "UPDATE_SETTINGS",
+              settings: JSON.parse(change.newValue)
+            });
+          }
+        }
+      }
+    }
+  );
+
+  return true;
+});
+
+chrome.runtime.onMessage.addListener(async (request, _sender, sendResponse) => {
+  if (request.method === "GET_SETTINGS") {
+    const storage = new Storage();
+    const settings = await storage.get("AWS_MASKING_SETTINGS");
+
+    sendResponse(settings);
+  }
+  return true;
+});
